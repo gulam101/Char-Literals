77621fda67d9fc72c62a01760d397175862a010c
unknown <gfram@hotmail.com>
2019-09-22 13:35:02 +0100

    first commit

---
+++ Char Litertals/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Char_Literals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char c = 'h';
+            char g = 'f';
+            char b = 'z';
+
+            Console.WriteLine(b);
+
+        }
+    }
+}
